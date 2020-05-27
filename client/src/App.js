fd7ff37b95c6683ee0eebbc68f45c475e16e0f0b
jvitela <jonathanvitela@Jonathans-MacBook-Pro.local>
2020-05-28 00:33:05 +0200

    Added client requests to create and fulfill order

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ky from "ky";
+import logo from "./logo.svg";
+import "./App.css";
+
+const api = ky.create({
+  prefixUrl: "https://r0ah3egg0d.execute-api.eu-central-1.amazonaws.com/dev",
+});
+
+async function createOrder() {
+  const json = {
+    items: [{ id: "item01", qty: 10 }],
+  };
+  try {
+    const order = await api.post("order", { json }).json();
+    console.info(order);
+    return order;
+  } catch (err) {
+    console.error(err);
+    return null;
+  }
+}
+
+async function fulfillOrder(orderId) {
+  const json = { orderId };
+  try {
+    const response = await api.put("order", { json }).json();
+    console.info(response);
+    return response.voucher;
+  } catch (err) {
+    console.error(err);
+    return null;
+  }
+}
 
 function App() {
+  async function onClick() {
+    const order = await createOrder();
+    if (order) {
+      await fulfillOrder(order.orderId);
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -19,6 +57,10 @@ function App() {
           Learn React
         </a>
       </header>
+      <div>
+        Create order:
+        <button onClick={onClick}>Create</button>
+      </div>
     </div>
   );
 }
